497c51e61390339678e39655b6ab59475d9155ac
Megan Smith <megansmith@dhcp-168-105-229-148.wireless.manoa.hawaii.edu>
2024-10-08 12:56:04 -1000

    Adding stuff

---
+++ Week_06/scripts/Week06a_lecture.R
@@ -0,0 +1,15 @@
+### Week 6a: Intro to RMarkdown
+### Created by: Megan Smith 
+### Created on: 2024-10-01
+##################################################
+
+### Load libraries #########
+library(tidyverse)
+library(here)
+library(rmarkdown)
+library(tinytex)
+library(beepr)
+
+### load data
+
+beep(7)
